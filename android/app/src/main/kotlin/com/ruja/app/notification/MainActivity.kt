4848872fa094e181c07258fba03b681d0aa889b5
rujabhatta22 <rujabhatta22@gmail.com>
2023-01-30 10:36:43 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/ruja/app/notification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ruja.app.notification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/notification.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text("Notification"),
+      ),
+    );
+  }
+}
